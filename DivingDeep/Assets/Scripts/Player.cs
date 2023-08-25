02656d03eed3e0f05cabe19fc2a32c6a14df512b
Mara <m.alsadoon90@gmail.com>
2023-08-25 00:49:29 -0400

    more updates to the player

--- DivingDeep/Assets/Scripts/Player.cs
+++ DivingDeep/Assets/Scripts/Player.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Transform _cameraTargetTransform;
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 5.0f;
+    private float playerSpeed = 15.0f;
     private float jumpHeight = 1.0f;
-    private float gravityValue = 0f;
+    private float gravityValue = -9f;
     private Animator anim;
     private Vector3 lastPos;
+    private bool isUnderWater = false;
 
     private void Start()
     {
@@ -23,8 +23,6 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-       // MousePosition = _camera.ScreenToWorldPoint(_playerInputController.actions.FindAction("Aim").ReadValue<Vector2>());
-        _cameraTargetTransform.position = ((Vector2)transform.position) / 2f;
 
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
@@ -32,17 +30,17 @@ public class Player : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),  Input.GetAxis("Vertical"),0);
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         if (move != Vector3.zero)
         {
             gameObject.transform.forward = move;
-           
+
         }
 
 
-        if (transform.position != lastPos)
+        if (transform.position != lastPos && !isUnderWater)
         {
             anim.SetBool("walk", true);
         }
@@ -51,21 +49,32 @@ public class Player : MonoBehaviour
             anim.SetBool("walk", false);
         }
 
+        if (transform.position != lastPos && isUnderWater)
+        {
+            anim.SetBool("swim", true);
+        }
+        else
+        {
+            anim.SetBool("swim", false);
+        }
+
         lastPos = transform.position;
 
         if (Input.GetKeyDown("space"))
         {
             anim.SetBool("walk", false);
             anim.SetBool("dive", true);
+            isUnderWater = true;
+            gravityValue = 0;
         }
 
-            /*// Changes the height position of the player..
-            if (Input.GetButtonDown("Jump") && groundedPlayer)
-            {
-                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-            }*/
+        /*// Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }*/
 
-            playerVelocity.y += gravityValue * Time.deltaTime;
+        playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
 
